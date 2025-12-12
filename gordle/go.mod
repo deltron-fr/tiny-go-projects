b82fe7ad1c4967d34b41d47a44eacb56184500f1
deltron-fr <kayceoogbonnaya2304@gmail.com>
2025-12-12 21:04:40 +0100

    initial commit for gordle

---
+++ gordle/go.mod
@@ -0,0 +1,3 @@
+module github.com/deltron-fr/tiny-go-projects/gordle
+
+go 1.24.4
---
+++ gordle/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/deltron-fr/tiny-go-projects/gordle/gordle"
+
+func main() {
+	g := gordle.New()
+	g.Play()
+}
