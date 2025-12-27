cbab60d08600cf63034c9dd2495b4d7ab3e9317d
deltron-fr <kayceoogbonnaya2304@gmail.com>
2025-12-27 16:36:03 +0100

    improved the cache and added a ttl

--- genericcache/cache.go
+++ genericcache/cache.go
@@ -1,34 +1,96 @@
 package genericcache
 
+import (
+	"slices"
+	"sync"
+	"time"
+)
+
 // Cache is key-value storage.
 type Cache[K comparable, V any] struct {
-	data map[K]V
+	ttl time.Duration
+
+	mu   sync.Mutex
+	data map[K]entryWithTimeout[V]
+
+	maxSize           int
+	chronologicalKeys []K
+}
+
+type entryWithTimeout[V any] struct {
+	value   V
+	expires time.Time
 }
 
 // New creates a usable Cache.
-func New[K comparable, V any]() Cache[K, V] {
+func New[K comparable, V any](maxSize int, ttl time.Duration) Cache[K, V] {
 	return Cache[K, V]{
-		data: make(map[K]V),
+		ttl:  ttl,
+		data: make(map[K]entryWithTimeout[V]),
+		maxSize: maxSize,
+		chronologicalKeys: make([]K, 0, maxSize),
 	}
 }
 
 // Read returns the associated value for a key,
 // and a boolean to true if the key is absent.
-func(c *Cache[K, V]) Read(key K) (V, bool) {
-	v, found := c.data[key]
-	return v, found
+func (c *Cache[K, V]) Read(key K) (V, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var zeroV V
+
+	e, ok := c.data[key]
+
+	switch {
+	case !ok:
+		return zeroV, false
+	case e.expires.Before(time.Now()):
+		c.deleteKeyValue(key)
+		return zeroV, false
+	default:
+		return e.value, true
+	}
 }
 
 // Upsert overrides the value for a given key.
 func (c *Cache[K, V]) Upsert(key K, value V) error {
-	c.data[key] = value
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	_, alreadyPresent := c.data[key]
+	switch {
+	case alreadyPresent:
+		c.deleteKeyValue(key)
+	case len(c.data) == c.maxSize:
+		c.deleteKeyValue(c.chronologicalKeys[0])
+	}
 
+	c.addKeyValue(key, value)
 	// Do not return an error for the moment,
 	// but it can happen in the near future.
 	return nil
 }
 
 // Delete removes the entry for the given key.
-func (c *Cache[K, V]) Delete(key K) {    
+func (c *Cache[K, V]) Delete(key K) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.deleteKeyValue(key)
+}
+
+// addKeyValue inserts a key and its value into the cache.
+func (c *Cache[K, V]) addKeyValue(key K, value V) {
+	c.data[key] = entryWithTimeout[V]{
+		value:   value,
+		expires: time.Now().Add(c.ttl),
+	}
+	c.chronologicalKeys = append(c.chronologicalKeys, key)
+}
+
+// deleteKeyValue removes a key and its associated value from the cache.
+func (c *Cache[K, V]) deleteKeyValue(key K) {
+	c.chronologicalKeys = slices.DeleteFunc(c.chronologicalKeys, func (k K) bool  {
+		return (k == key)
+	})
 	delete(c.data, key)
 }
--- genericcache/go.mod
+++ genericcache/go.mod
@@ -1,3 +1,11 @@
-module github.com/deltron-fr/tiny-go-projects
+module github.com/deltron-fr/tiny-go-projects/genericcache
 
 go 1.24.4
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+	golang.org/x/exp v0.0.0-20251219203646-944ab1f22d93 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
